2bc34a649a3d1cb1b510de0970abead7a0a37192
tookdes <38042623+tookdes@users.noreply.github.com>
2026-09-08 17:03:26 +0800

    Use distinctly named packaged version helper

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@
 require_relative "boot"
 
 require "rails/all"
-require_relative "../lib/version"
+require_relative "../lib/pwpush_packaged_version"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
