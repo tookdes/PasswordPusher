e4d50d82adebbdb9a519c3fedc4f457a60ff50f2
tookdes <38042623+tookdes@users.noreply.github.com>
2026-09-08 17:05:19 +0800

    Load packaged version helper from config

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@
 require_relative "boot"
 
 require "rails/all"
-require_relative "../lib/pwpush_packaged_version"
+require_relative "pwpush_version"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
